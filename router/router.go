72b9a0257dcea311ddc6a183187f65292c3bd6e5
anudeep-mp <m.anudeep2000@gmail.com>
2023-04-28 22:29:29 +0530

    Removed tracker apis and moved to a different project

--- router/router.go
+++ router/router.go
@@ -12,9 +12,6 @@ func Router() *mux.Router {
 	router.HandleFunc("/api/sendmessage", controller.SendMessageHandler).Methods("POST")
 	router.HandleFunc("/api/messages", controller.GetMessagesHandler).Methods("GET")
 	router.HandleFunc("/api/messages", controller.DeleteAllMessagesHandler).Methods("DELETE")
-	router.HandleFunc("/api/track", controller.WatchStampHandler).Methods("POST")
-	router.HandleFunc("/api/watchstamps", controller.GetWatchStampsHandler).Methods("GET")
-	router.HandleFunc("/api/watchstamps", controller.DeleteAllWatchStampsHandler).Methods("DELETE")
 
 	return router
 }
